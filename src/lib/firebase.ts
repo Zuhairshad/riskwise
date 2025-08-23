cea61b09152b99e2768dd3a3ff5045e72fb3a606
zuhayr shad <zuhairshad140@gmail.com>
2025-08-23 10:52:56 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8Y57s-aUAIMU51D8oYOMmHGSa0K7Y1jY",
-  authDomain: "riskwise-c9df3.firebaseapp.com",
-  projectId: "riskwise-c9df3",
-  storageBucket: "riskwise-c9df3.appspot.com",
-  messagingSenderId: "290153782628",
-  appId: "1:290153782628:web:e9eb93421d972053da6e71",
-  measurementId: "G-EMYKZN4N92"
+  "projectId": "riskwise-l2td9",
+  "appId": "1:954229157369:web:26d2fe18decfaaa5176128",
+  "storageBucket": "riskwise-l2td9.appspot.com",
+  "apiKey": "AIzaSyBlh8BoBA3L4E4Uk6AKRCoHGxCb38cuPVQ",
+  "authDomain": "riskwise-l2td9.firebaseapp.com",
+  "messagingSenderId": "954229157369"
 };
 
 // Initialize Firebase
