adfaf030febd672bb99d340fb7e4ae9180b62f05
zuhayr shad <zuhairshad140@gmail.com>
2025-08-26 10:07:39 +0000

    https://console.firebase.google.com/project/riskwise-c9df3/firestore/dat

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,14 @@
+
 import { initializeApp, getApp, getApps, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
 import { getFirestore, type Firestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyDlDFgpTAxkPibgdDs-vFuXA0Vy1ny5A",
-  authDomain: "proactify-99042.firebaseapp.com",
-  projectId: "proactify-99042",
-  storageBucket: "proactify-99042.appspot.com",
+  apiKey: "AIzaSyDFt5n5Yq2x_o3qV2eC3kHj9FpZ-iBxR0c",
+  authDomain: "riskwise-c9df3.firebaseapp.com",
+  projectId: "riskwise-c9df3",
+  storageBucket: "riskwise-c9df3.appspot.com",
   messagingSenderId: "325492193792",
   appId: "1:325492193792:web:72895204439c235774a353",
   measurementId: "G-L0M2GV36E1"
