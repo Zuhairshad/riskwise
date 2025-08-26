470e9e495d83167d220f69e7078b419326d9780f
uzair ahmad <uzair11ahmad@gmail.com>
2025-08-26 07:29:51 +0000

    AI suggestionis not working use AIzaSyDjpBvQ6ixJjyZbXFJ4er9Ow8Du4dSb7Cs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,10 @@
 import { initializeApp, getApp, getApps, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
 import { getFirestore, type Firestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyDlDFgpTAxkPibgdDs-vFuXA0Vy1ny5A",
+  apiKey: "AIzaSyDjpBvQ6ixJjyZbXFJ4er9Ow8Du4dSb7Cs",
   authDomain: "proactify-99042.firebaseapp.com",
   projectId: "proactify-99042",
   storageBucket: "proactify-99042.appspot.com",
@@ -17,5 +18,6 @@ const firebaseConfig = {
 const app: FirebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth: Auth = getAuth(app);
 const db: Firestore = getFirestore(app);
+const storage = getStorage(app);
 
-export { db, auth, app };
+export { db, auth, app, storage };
