b39ce3cb80133c524023bc08529e04992460d149
zuhayr shad <zuhairshad140@gmail.com>
2025-08-27 05:57:31 +0000

    still not

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -12,7 +12,7 @@ export default function AuthLayout({
                 <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
                 <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
             </div>
-            <main className="flex-1 flex flex-col items-center justify-center p-4 sm:p-6 lg:p-8">
+            <main className="flex-1 flex items-center justify-center p-4 sm:p-6 lg:p-8">
                 <div className="w-full max-w-md">
                     {children}
                 </div>
