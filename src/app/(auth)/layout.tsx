58c629ac5dbe6cb4020e2364f6a0639aa91150c6
zuhayr shad <zuhairshad140@gmail.com>
2025-08-27 05:58:15 +0000

    still not in center

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ export default function AuthLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="relative flex min-h-screen flex-col items-center justify-center">
             <div className="fixed inset-0 -z-10 h-full w-full bg-background">
                 <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
                 <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
@@ -17,7 +17,7 @@ export default function AuthLayout({
                     {children}
                 </div>
             </main>
-            <footer className="py-4 text-center text-xs text-muted-foreground">
+            <footer className="w-full py-4 text-center text-xs text-muted-foreground">
                 <p>Design by Uzair Ahmad</p>
             </footer>
         </div>
