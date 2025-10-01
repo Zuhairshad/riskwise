bce2e5f4b925195d15557c4a9403f0ba540a3a8e
uzair ahmad <uzair11ahmad@gmail.com>
2025-10-01 04:55:02 +0000

    use the key only for AI services and rest back to last change

--- src/ai/client.ts
+++ src/ai/client.ts
@@ -6,7 +6,7 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [
     googleAI({
-        apiKey: process.env.GEMINI_API_KEY,
+        apiKey: 'AIzaSyAQUuVdVhO_kHO3obrbsnGHRGpM0ObA-oM',
     }),
   ],
 });
